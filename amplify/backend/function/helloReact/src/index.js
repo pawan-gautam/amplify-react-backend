3145cf03ddc3c74328686b87684aad50c1d1c203
pawan gautam <nickelfox@pop-os.localdomain>
2024-01-16 23:41:27 +0530

    test

--- amplify/backend/function/helloReact/src/index.js
+++ amplify/backend/function/helloReact/src/index.js
@@ -15,6 +15,6 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-        body: JSON.stringify('test 123'),
+        body: JSON.stringify('test 123 123333'),
     };
 };
