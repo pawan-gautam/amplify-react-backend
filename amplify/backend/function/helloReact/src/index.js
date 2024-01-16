1a99fe3c6007063b46f3ce7953d8766ba4c62a4d
pawan gautam <nickelfox@pop-os.localdomain>
2024-01-16 17:05:38 +0530

    minor changes

--- amplify/backend/function/helloReact/src/index.js
+++ amplify/backend/function/helloReact/src/index.js
@@ -15,6 +15,6 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-        body: JSON.stringify('hello world testing112122222'),
+        body: JSON.stringify('hello world testing1121222222312455'),
     };
 };
